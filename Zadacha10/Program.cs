30d4b9ef007f0bfb2f1cd488b256debb9f7dbacc
Elya Shabanova <elya_aralbaeva@mail.ru>
2022-10-18 14:38:44 +0600

    Zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+
+
+
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 100;
+int secondDigit = (number-(firstDigit*100)) / 10; 
+
+Console.WriteLine($"Полученное число {secondDigit} ");
