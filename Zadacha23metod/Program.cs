395ec994330eae0c03f2c2589263ebebb0c555fe
Elya Shabanova <elya_aralbaeva@mail.ru>
2022-10-25 13:24:23 +0600

    Zadacha23metod

---
+++ Zadacha23metod/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает 
+// 1. на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+void SquareTable(int num) 
+{
+    if(num > 0)
+    {
+        for (int i = 1; i <= num; i++) 
+        {
+            Console.WriteLine($"{i} | {i * i * i}"); 
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите значение больше 0 ");
+    }
+}
+
+SquareTable(n);
