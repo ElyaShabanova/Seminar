109840c3290efdb1675b19106a5e53cbf40fb6f3
Elya Shabanova <elya_aralbaeva@mail.ru>
2022-10-25 13:07:16 +0600

    Zadacha23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает 
+// 1. на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите данные:"); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+int count=1; 
+while (count <=n) 
+{
+    int quad = count*count*count; 
+    Console.WriteLine($"{count}  ->  {quad}");  
+    count++;  
+}
