80e149fba07f9d4b99f64cce533f33a6abec77c8
Elya Shabanova <elya_aralbaeva@mail.ru>
2022-11-08 17:17:46 +0600

    Zadacha29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая задаёт
+массив из 8 элементов, заполненный
+псевдослучайными числами и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.WriteLine("Введите количество элементов массива : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] NameArray(int size)
+{
+    int[] array = new int[size]; //     
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 99);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+}
+int[] nameArray = NameArray(num);
+PrintArray(nameArray);
