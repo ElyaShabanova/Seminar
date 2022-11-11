63b979181ec0ec022c21aa55bf0e9c2a1a193aef
Elya Shabanova <elya_aralbaeva@mail.ru>
2022-11-12 00:11:23 +0600

    Zadacha34

---
+++ Zadacha34/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в
+массиве.*/
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] number = new int[size];
+FillArrayRandomNumbers(number);
+PrintArray(number);
+int count = 0;
+
+for (int a = 0; a < number.Length; a++)
+{
+    if (number[a] % 2 == 0)
+        count++;
+}
+
+Console.WriteLine($"Четных чисел {count} ");
+
+
+void FillArrayRandomNumbers(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+    Console.WriteLine();
+}
+
