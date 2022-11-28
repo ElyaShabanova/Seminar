51f3a4f4f6b88bb77da07922313c6f8c6577c8fc
Elya Shabanova <elya_aralbaeva@mail.ru>
2022-11-28 11:54:32 +0600

    Zadacha64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все
+//натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+
+
+
+
+
+void PrintInterval(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = m;
+				end = n;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		PrintInterval(1, 8);
+		
+		Console.WriteLine();
